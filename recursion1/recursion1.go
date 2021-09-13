020faec9a1e84f950d816dacb7baea01471beb4f
sderohan <rohanshinde7212@gmail.com>
2021-09-13 11:45:55 +0530

    add bunnyEars2

--- recursion1/recursion1.go
+++ recursion1/recursion1.go
@@ -15,3 +15,23 @@ func factorial(n int) int {
 	}
 	return n * factorial(n-1)
 }
+
+/*
+We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears.
+The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot.
+Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
+
+bunnyEars2(0) → 0
+bunnyEars2(1) → 2
+bunnyEars2(2) → 5
+*/
+
+func bunnyEars2(n int) int {
+	if n == 0 {
+		return n
+	}
+	if n%2 == 0 {
+		return 3 + bunnyEars2(n-1)
+	}
+	return 2 + bunnyEars2(n-1)
+}
--- recursion1/recursion1_test.go
+++ recursion1/recursion1_test.go
@@ -23,3 +23,24 @@ func TestFactorial(t *testing.T) {
 		}
 	}
 }
+
+func TestBunnyEars2(t *testing.T) {
+	var tests = []struct {
+		n    int
+		want int
+	}{
+		{0, 0},
+		{1, 2},
+		{2, 5},
+		{3, 7},
+		{4, 10},
+		{5, 12},
+		{6, 15},
+		{10, 25},
+	}
+	for _, test := range tests {
+		if got := bunnyEars2(test.n); got != test.want {
+			t.Errorf("bunnyEars2(%v) = %v [wanted: %v]", test.n, got, test.want)
+		}
+	}
+}
